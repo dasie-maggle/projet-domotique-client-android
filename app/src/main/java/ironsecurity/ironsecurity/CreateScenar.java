3993ef9f39a8ccc3f3c567d122186c60b89c37f3
KarminMalkey <romain.vanoye@gmail.com>
2019-04-01 11:06:38 +0200

    p

--- app/src/main/java/ironsecurity/ironsecurity/CreateScenar.java
+++ app/src/main/java/ironsecurity/ironsecurity/CreateScenar.java
@@ -2,11 +2,24 @@ package ironsecurity.ironsecurity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class CreateScenar extends AppCompatActivity {
+
+    private ListView Action;
+    private String[] actions = new String[] {
+            "Allumer", "Eteindre"
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.creerscenario);
+
+        Action = (ListView) findViewById(R.id.action);
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(CreateScenar.this, android.R.layout.simple_list_item_1, actions);
+        Action.setAdapter(adapter);
     }
 }
