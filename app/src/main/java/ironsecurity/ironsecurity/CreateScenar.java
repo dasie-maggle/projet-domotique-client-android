ce6bfb782cc7f80f18c30c0a9bb9e79a29cdf632
KarminMalkey <romain.vanoye@gmail.com>
2019-04-01 15:02:02 +0200

    ajout spinner sur creer scenario

--- app/src/main/java/ironsecurity/ironsecurity/CreateScenar.java
+++ app/src/main/java/ironsecurity/ironsecurity/CreateScenar.java
@@ -1,25 +1,38 @@
 package ironsecurity.ironsecurity;
 
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class CreateScenar extends AppCompatActivity {
-
-    private ListView Action;
-    private String[] actions = new String[] {
-            "Allumer", "Eteindre"
-    };
+public class CreateScenar extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+    String[] actions = {"Choisir une action", "Allumer", "Eteindre"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.creerscenario);
 
-        Action = (ListView) findViewById(R.id.action);
+        Spinner spin = findViewById(R.id.Actions);
+        spin.setOnItemSelectedListener(this);
 
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(CreateScenar.this, android.R.layout.simple_list_item_1, actions);
-        Action.setAdapter(adapter);
+        ArrayAdapter aa= new ArrayAdapter(this, android.R.layout.simple_spinner_item, actions);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spin.setAdapter(aa);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+        Toast.makeText(getApplicationContext(),actions[position], Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?>arg0) {
+
     }
 }
