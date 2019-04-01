191a12feb3bb29fe18e11b0fd306b4fbefcd29d0
jdimont <j.dimont@laposte.net>
2019-04-01 11:02:38 +0200

    voilou

--- app/src/main/java/ironsecurity/ironsecurity/Mozaique.java
+++ app/src/main/java/ironsecurity/ironsecurity/Mozaique.java
@@ -8,6 +8,6 @@ public class Mozaique extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mozaique);
+        setContentView(R.layout.mosaique);
     }
 }
