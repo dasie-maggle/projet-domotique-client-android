bcdf7737873203cbd82733a1b6c3c847ea7fda87
Julien83500 <djulien@rouviere-gretavarmed.fr>
2019-04-02 09:52:07 -1200

    creer scenario ihm ok

--- app/src/main/java/ironsecurity/ironsecurity/ChoixScenario.java
+++ app/src/main/java/ironsecurity/ironsecurity/ChoixScenario.java
@@ -1,38 +1,43 @@
 package ironsecurity.ironsecurity;
 
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 public class ChoixScenario extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
-    String[] action = {"Choisir un scenario", "scenario 1", "scenario 2"};
+    private static String[] actions = {"-Choisir une action-","Allumer","Eteindre"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.choixscenario);
 
-        Spinner spin = findViewById(R.id.choix);
+        // Récupération des IDs
+        Spinner spin = (Spinner)findViewById(R.id.choix);
         spin.setOnItemSelectedListener(this);
 
-        ArrayAdapter<String> adapter= new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, action);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        spin.setAdapter(adapter);
+        // Remplissage des spinners
+        ArrayAdapter<String> aa=new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, actions);
+        spin.setAdapter(aa);
+
+
+        // Définition des écouteurs
+        spin.setOnItemSelectedListener(this);
+
     }
 
     @Override
-    public void onItemSelected(AdapterView<?>arg0, View arg1, int position, long id) {
-        Toast.makeText(getApplicationContext(),action[position], Toast.LENGTH_LONG).show();
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+        String selected =(String) parent.getAdapter().getItem(position);
     }
 
     @Override
-    public void onNothingSelected(AdapterView<?>arg0) {
+    public void onNothingSelected(AdapterView<?>arg0){
 
     }
 }
