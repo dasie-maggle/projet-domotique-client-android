0b614b9caa4ae22032250a25c82df0978a99464d
Zeiiy <j.dimont19@gmail.com>
2019-04-02 11:26:17 +0200

    ENFIN

--- app/src/main/java/ironsecurity/ironsecurity/Stat.java
+++ app/src/main/java/ironsecurity/ironsecurity/Stat.java
@@ -2,12 +2,37 @@ package ironsecurity.ironsecurity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class Stat extends AppCompatActivity {
+public class Stat extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.choixscenario);
-    }
+        Spinner spinner;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.choixscenario);
+
+            spinner = (Spinner) findViewById(R.id.choix);
+            ArrayAdapter<CharSequence> adapter =  ArrayAdapter.createFromResource(this, R.array.ChoixScenar, android.R.layout.simple_spinner_item);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner.setAdapter(adapter);
+            spinner.setOnItemSelectedListener(this);
+        }
+
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            String text = parent.getItemAtPosition(position).toString();
+            Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+
+
 }
