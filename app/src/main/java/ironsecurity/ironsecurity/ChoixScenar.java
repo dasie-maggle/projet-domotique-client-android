7a76d57c3f037a2bbad5cf253bd7ef23178ab715
Zeiiy <j.dimont19@gmail.com>
2019-04-02 13:01:40 +0200

    jpp

--- app/src/main/java/ironsecurity/ironsecurity/ChoixScenar.java
+++ app/src/main/java/ironsecurity/ironsecurity/ChoixScenar.java
@@ -2,11 +2,14 @@ package ironsecurity.ironsecurity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
+import android.view.Menu;
 
 public class ChoixScenar extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
@@ -16,13 +19,34 @@ public class ChoixScenar extends AppCompatActivity implements AdapterView.OnItem
         super.onCreate(savedInstanceState);
         setContentView(R.layout.choixscenario);
 
+
         spinner = (Spinner) findViewById(R.id.choix);
         ArrayAdapter<CharSequence> adapter =  ArrayAdapter.createFromResource(this, R.array.ChoixScenar, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add:
+
+                return true;
+            case R.id.action_delete:
+
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         String text = parent.getItemAtPosition(position).toString();
