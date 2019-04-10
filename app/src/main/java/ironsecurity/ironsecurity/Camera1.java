5e4b478e3fca3a8e50eb5d0d501e7ac781093e6a
apocarpio <48240773+apocarpio@users.noreply.github.com>
2019-04-10 08:57:09 +0200

    Camera Control Complete Code

--- app/src/main/java/ironsecurity/ironsecurity/Camera1.java
+++ app/src/main/java/ironsecurity/ironsecurity/Camera1.java
@@ -1,49 +1,88 @@
-package ironsecurity.ironsecurity;
-
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.webkit.URLUtil;
-import android.webkit.WebSettings;
+import android.view.View;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.widget.Button;
 
-public class Camera1 extends AppCompatActivity {
-    private static final String URL = "http://admin:@172.16.3.14/video.cgi";
-    private WebView mWebView;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+import ironsecurity.ironsecurity.R;
 
-    /**
-     *
-     * @param savedInstanceState
-     */
+class MainActivity extends AppCompatActivity {
+
+    Button btnUp;
+    Button btnDown;
+    Button btnLeft;
+    Button btnRight;
+    WebView webView;
+    String urlUp;
+    String urlDown;
+    String urlLeft;
+    String urlRight;
+    URL url;
+    HttpURLConnection urlConnection;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.camera1);
 
-        mWebView = (WebView) findViewById(R.id.camera1);
+        btnUp = findViewById( R.id.btnUp);
+        btnDown = findViewById(R.id.btnDown);
+        btnLeft = findViewById(R.id.btnLeft);
+        btnRight = findViewById(R.id.btnRight);
+        webView = findViewById(R.id.webView);
 
-        mWebView.setWebViewClient(new WebViewClient() {
+        urlUp = "http://172.16.3.14/pantiltcontrol.cgi?PanSingleMoveDegree=5&TiltSingleMoveDegree=5&PanTiltSingleMove=1";
+        urlDown = "http://172.16.3.14/pantiltcontrol.cgi?PanSingleMoveDegree=5&TiltSingleMoveDegree=5&PanTiltSingleMove=7";
+        urlLeft = "http://172.16.3.14/pantiltcontrol.cgi?PanSingleMoveDegree=5&TiltSingleMoveDegree=5&PanTiltSingleMove=3";
+        urlRight = "http://172.16.3.14/pantiltcontrol.cgi?PanSingleMoveDegree=5&TiltSingleMoveDegree=5&PanTiltSingleMove=5";
+
+        try {
+            url = new URL("http://172.16.3.14/pantiltcontrol.cgi?PanSingleMoveDegree=5&TiltSingleMoveDegree=5&PanTiltSingleMove=1");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+
+        webView.loadUrl("http://admin:@172.16.3.14/video.cgi");
+
+        btnUp.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                if (url == null || url.startsWith("http://") || url.startsWith("https://")) return false;
-
-                try {
-                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-                    view.getContext().startActivity(intent);
-                    return true;
-                } catch (Exception e) {
-                    return true;
-                }
+            public void onClick(View v) {
+                webView.loadUrl(urlUp);
             }
         });
 
-        WebSettings ws = mWebView.getSettings();
-        ws.setJavaScriptEnabled(true);
-        mWebView.loadUrl(URL);
+        btnDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //requestQueue.add(requestDown);
+                webView.loadUrl(urlDown);
+            }
+        });
+
+        btnLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                webView.loadUrl(urlLeft);
+            }
+        });
+
+        btnRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                webView.loadUrl(urlRight);
+            }
+        });
     }
+
+
+
+
+
+
+
 }
